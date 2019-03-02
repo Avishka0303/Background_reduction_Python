4ebecee1e9b968809cf4042c9deed83f1d192018
Avishka Dhanajaya <avishka0303@gmail.com>
2019-03-02 21:46:02 +0530

    general mog reduction applied

--- BackGroundReduction/__init__.py
+++ BackGroundReduction/__init__.py
@@ -1,1 +1,22 @@
+import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+
+#cap=cv2.VideoCapture('people-walking.mp4')
+cap=cv2.VideoCapture(0)
+fgbg=cv2.createBackgroundSubtractorMOG2()
+
+#face tracking concept.
+while True:
+    ret, frame=cap.read()
+    fgmask=fgbg.apply(frame)
+
+    cv2.imshow('original',frame)
+    cv2.imshow('fg',fgmask)
+
+    k=cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
